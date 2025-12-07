d64a004d398d4dae12ea92800942c79a993d2b7c
= <ficho-here@gmail.com>
2025-12-07 11:28:39 +0100

    Fixed some error's with ledge spawning

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,10 +2,15 @@ extends Node2D
 
 
 @onready var ledge = $map1/Area2D 
-
+@onready var map1 = $map1
 
 func _ready() -> void:
-	create_ledge(0, 0, 100)	
+	
+	var first_ledge = ledge.duplicate()
+	first_ledge.global_position = Vector2(0, -200)
+	map1.add_child(first_ledge)
+	
+	create_ledge(0, -200, 100)
 
 
 func create_ledge(xp: float, yp: float, count: int):
@@ -13,21 +18,16 @@ func create_ledge(xp: float, yp: float, count: int):
 	var randpos = randi_range(1, 2)
 	if count == 0:
 		return
-		
+			
 	var another_ledge = ledge.duplicate()
 	
-	if count == count:
-		
-		another_ledge.global_position = Vector2(0, -100)
-		add_child(another_ledge)
-		
 	if randpos == 1:
 		another_ledge.global_position = Vector2(xp+100, yp-100)
-		add_child(another_ledge)
+		map1.add_child(another_ledge)
 		create_ledge(xp+100, yp-100, count-1)
 	else: 
 		another_ledge.global_position = Vector2(xp-100, yp-100)
-		add_child(another_ledge)
+		map1.add_child(another_ledge)
 		create_ledge(xp-100, yp-100, count-1)
 		
 	
